1acf2596b2f326a0cf2f1a689226ed59165834d1
wyattlindsey <wyatt@spatially.com>
2018-05-28 10:17:52 -0700

    start scaffolding image processing

--- server/handlers/image.js
+++ server/handlers/image.js
@@ -1,5 +1,27 @@
-const upload = (req, res) => {
-  res.sendStatus(201)
+import CannyEdgeDetector from 'canny-edge-detector'
+import Image from 'image'
+
+import dotProp from 'dot-prop'
+
+const upload = async (req, res) => {
+  let status = 201
+  const imagePath = dotProp.get(req, 'file.path')
+  const fileName = dotProp.get(req, 'file.filename')
+
+  if (!imagePath) {
+    status = 500
+  } else {
+    try {
+      const loadedImage = await Jimp.read(imagePath)
+      const grey = loadedImage.greyscale()// write(`public/uploads/${fileName}-greyscale.jpg`)
+      const edge = CannyEdgeDetector(loadedImage)
+    } catch (e) {
+      console.error('Image processing failed', e)
+      status = 500
+    }
+  }
+
+  res.sendStatus(status)
 }
 
 export default {
