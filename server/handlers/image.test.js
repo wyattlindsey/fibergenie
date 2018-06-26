18680e79c0d3e543c025a77f863abfa21695d908
wyattlindsey <wyatt@spatially.com>
2018-06-26 08:58:04 -0700

    finish handler tests for now

--- server/handlers/image.test.js
+++ server/handlers/image.test.js
@@ -22,22 +22,36 @@ const unlinkSyncStub = sinon.stub(fs, 'unlinkSync')
 const writeFileSyncStub = sinon.stub(fs, 'writeFileSync')
 
 // lib/images.js stubs
-const convertPDFStub = sinon.stub(Image, 'convertPDF').resolves([{}, {}])
-const getDimensionsStub = sinon.stub(Image, 'getDimensions').resolves(100, 100)
-const prepareStub = sinon.stub(Image, 'prepare').resolves(UPLOADS_FOLDER)
-const resizeStub = sinon.stub(Image, 'resize').resolves(UPLOADS_FOLDER)
-const saveCopyStub = sinon.stub(Image, 'saveCopy').resolves(UPLOADS_FOLDER)
+const convertPDFStub = sinon.stub(Image, 'convertPDF')
+const getDimensionsStub = sinon.stub(Image, 'getDimensions')
+const prepareStub = sinon.stub(Image, 'prepare')
+const resizeStub = sinon.stub(Image, 'resize')
+const saveCopyStub = sinon.stub(Image, 'saveCopy')
 
 // lib/chart.js stubs
-const extractLinesStub = sinon.stub(Chart, 'extractLines').returns({})
+const extractLinesStub = sinon.stub(Chart, 'extractLines')
 
 // Express stubs
 const response = {
   send: noop,
   status: noop,
 }
-const responseStatusStub = sinon.stub(response, 'status').returns(response)
-const responseSendStub = sinon.stub(response, 'send').returns(response)
+const responseStatusStub = sinon.stub(response, 'status')
+const responseSendStub = sinon.stub(response, 'send')
+
+const chartData = {
+  boundingBox: {
+    p1: {
+      x: 1,
+      y: 1,
+    },
+    p2: {
+      x: 100,
+      y: 100,
+    },
+  },
+  rowPositions: [1, 10, 19, 28, 37, 46],
+}
 
 describe('image upload', () => {
   before(() => {})
@@ -50,10 +64,24 @@ describe('image upload', () => {
     writeFileSyncStub.restore()
 
     convertPDFStub.restore()
+    getDimensionsStub.restore()
+    prepareStub.restore()
+    resizeStub.restore()
+    saveCopyStub.restore()
     extractLinesStub.restore()
   })
 
-  beforeEach(() => {})
+  beforeEach(() => {
+    convertPDFStub.resolves([{}, {}, {}]) // creates three separate pages
+    getDimensionsStub.resolves(100, 100)
+    prepareStub.resolves(UPLOADS_FOLDER)
+    resizeStub.resolves(UPLOADS_FOLDER)
+    saveCopyStub.resolves(UPLOADS_FOLDER)
+    extractLinesStub.returns(chartData)
+
+    responseStatusStub.returns(response)
+    responseSendStub.returns(response)
+  })
 
   afterEach(() => {
     mkdirSyncStub.reset()
@@ -62,6 +90,11 @@ describe('image upload', () => {
     unlinkSyncStub.reset()
     writeFileSyncStub.reset()
 
+    convertPDFStub.reset()
+    getDimensionsStub.reset()
+    prepareStub.reset()
+    resizeStub.reset()
+    saveCopyStub.reset()
     extractLinesStub.reset()
 
     responseStatusStub.reset()
@@ -118,7 +151,18 @@ describe('image upload', () => {
     expect(convertPDFStub.called).to.equal(false)
   })
 
-  it('creates multiple pages if multi-page document')
+  it('creates folders for each PDF page processed and creates a file in that directory', async () => {
+    const pdf = { ...fileToUpload, ...pdfMimeType }
 
-  it('responds with chart data on success', () => {})
+    await imageHandler.processUpload(pdf, UPLOADS_FOLDER) // convertPDF stub resolves 3 pages
+    expect(mkdirSyncStub.callCount).to.equal(2) // page_1 folder already exists so 2 more should be created
+    expect(saveCopyStub.callCount).to.equal(3) // total of 3 png copies created
+  })
+
+  it('responds with chart data and 201 status code on success', async () => {
+    const request = { file: fileToUpload }
+    await imageHandler.upload(request, response)
+    expect(responseStatusStub.calledWith(201)).to.equal(true)
+    expect(responseSendStub.calledWith([chartData])).to.equal(true)
+  })
 })
